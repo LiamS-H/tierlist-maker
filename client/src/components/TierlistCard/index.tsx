e610fb52913f9d31ef4d7406568456b6de37ac3f
liam-sh <liamsh@gmail.com>
2023-12-07 02:33:02 -0800

    added visibility to cards

--- client/src/components/TierlistCard/index.tsx
+++ client/src/components/TierlistCard/index.tsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
-import { Tierlist } from "../../models/tierlist";
-import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
+import { ITierlist } from "../../models/tierlist";
+import { Box, Card, CardActionArea, CardContent, Typography } from '@mui/material';
+import LockIcon from '@mui/icons-material/Lock'
+import PublicIcon from '@mui/icons-material/Public'
 
-export default function TierlistCard(props: {tierlist: Tierlist}) {
+export default function TierlistCard(props: {tierlist: ITierlist}) {
     return (<>
     <Card>
         <CardActionArea LinkComponent={Link} to={`/tierlist/${props.tierlist._id}`}>
             <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-                {props.tierlist.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-                {props.tierlist.owner}
-            </Typography>
+                <Box sx={{display:"flex",flexFlow:"row"}}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {props.tierlist.name}
+                    </Typography>
+                    {props.tierlist.visibility == "Public"?<PublicIcon/>:<LockIcon/>}
+                </Box>
+                <Typography variant="body2" color="text.secondary">
+                    {props.tierlist.owner}
+                </Typography>
             </CardContent>
         </CardActionArea>
 
