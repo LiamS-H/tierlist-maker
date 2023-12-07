222911da36007d29627aba841a15c7d238914457
liam-sh <liamsh@gmail.com>
2023-12-07 02:33:30 -0800

    refactor group hooks and added new ones

--- client/src/hooks/tierlists.ts
+++ client/src/hooks/tierlists.ts
@@ -1,6 +1,41 @@
 import { useState, useEffect } from 'react'
 import AXIOS from 'axios'
 import { ITierlist } from '../models/tierlist';
+import { useUser } from './auth';
+
+function useSharedTierlists() {
+    const [tierlists, setTierlists] = useState<ITierlist[]>([]);
+    const user = useUser()
+
+    useEffect(() => {
+        if (user == null) return;
+        AXIOS.get(`${import.meta.env.VITE_SERVER_ENDPOINT}/api/tierlists/shared`,{
+            params: {
+                token:user.user_uuid
+            }
+        })
+        .then((response) => setTierlists(response.data))
+    }, [user]);
+  
+    return tierlists;
+}
+
+function useUserTierlists() {
+    const [tierlists, setTierlists] = useState<ITierlist[]>([]);
+    const user = useUser()
+
+    useEffect(() => {
+        if (user == null) return;
+        AXIOS.get(`${import.meta.env.VITE_SERVER_ENDPOINT}/api/tierlists/user`,{
+            params: {
+                token:user.user_uuid
+            }
+        })
+        .then((response) => setTierlists(response.data))
+    }, [user]);
+  
+    return tierlists;
+}
 
 function usePublicTierlists() {
     const [tierlists, setTierlists] = useState<ITierlist[]>([]);
@@ -10,7 +45,24 @@ function usePublicTierlists() {
         .then((response) => setTierlists(response.data))
     }, []);
   
-    return { tierlists };
+    return tierlists;
 }
 
-export { usePublicTierlists }
+function useSearchPublicTierlists() {
+    const [tierlists, setTierlists] = useState<ITierlist[]>([]);
+    const [query, setQuery] = useState<string>("");
+  
+    useEffect(() => {
+        AXIOS.get(`${import.meta.env.VITE_SERVER_ENDPOINT}/api/tierlists/search`,{
+            params: {
+                search:query
+            }
+        })
+        .then((response) => setTierlists(response.data))
+    }, [query]);
+  
+    return { tierlists, query, setQuery  };
+}
+
+
+export { usePublicTierlists, useSearchPublicTierlists, useSharedTierlists, useUserTierlists}
