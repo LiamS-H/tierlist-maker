f3e1d4288d6794dd17cf3b9a8729e30b59e24486
liam-sh <liamsh@gmail.com>
2023-12-07 02:34:00 -0800

    Home now displays personal tierlists

--- client/src/routes/home/index.tsx
+++ client/src/routes/home/index.tsx
@@ -1,10 +1,14 @@
 import TierlistCardList from "../../components/TierlistCardList"
-import { usePublicTierlists } from "../../hooks/tierlists"
+import { useUser } from "../../hooks/auth"
+import { usePublicTierlists, useUserTierlists } from "../../hooks/tierlists"
 
 export default function Home() {
-    const { tierlists } = usePublicTierlists()
+    const tierlists = usePublicTierlists()
+    const userTierlists = useUserTierlists()
+    const user = useUser()
+    
     return (<>
-        <h1>Home</h1>
-        <TierlistCardList tierlists={tierlists} />
+        {user?<h1>{user.username}'s Tierlists</h1>:<h1>Home</h1>}
+        <TierlistCardList tierlists={user?userTierlists:tierlists} />
     </>)
 }
