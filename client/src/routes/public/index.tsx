459b0adf5f9ae1002dae831a991efc9b455016f3
liam-sh <liamsh@gmail.com>
2023-12-09 20:30:54 -0800

    Add search bar to public

--- client/src/routes/public/index.tsx
+++ client/src/routes/public/index.tsx
@@ -1,11 +1,21 @@
 import TierlistCardList from "../../components/TierlistCardList"
-import { usePublicTierlists } from "../../hooks/tierlists"
+import { useSearchPublicTierlists } from "../../hooks/tierlists"
+import { TextField } from "@mui/material"
 
 export default function Public() {
-    const tierlists = usePublicTierlists()
+    const {tierlists, query, setQuery } = useSearchPublicTierlists()
+    
     
     return (<>
         <h1>Public Tierlists</h1>
+        <TextField
+            label="search"
+            value={query}
+            variant="outlined"
+            onChange={(e)=>{
+                setQuery(e.target.value)
+            }}
+        />
         <TierlistCardList tierlists={tierlists} />
     </>)
 }
