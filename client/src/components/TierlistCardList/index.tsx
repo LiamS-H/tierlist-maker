947314d96f0126b6f36486bee777c9338a6c06d3
liam-sh <liamsh@gmail.com>
2023-12-09 20:30:32 -0800

    message for 0 tierlists to display

--- client/src/components/TierlistCardList/index.tsx
+++ client/src/components/TierlistCardList/index.tsx
@@ -1,7 +1,9 @@
-import { Tierlist } from "../../models/tierlist";
+import { Typography } from "@mui/material";
+import { ITierlist } from "../../models/tierlist";
 import TierlistCard from "../TierlistCard";
 
-export default function TierlistCardList(props: {tierlists: Tierlist[]}) {
+export default function TierlistCardList(props: {tierlists: ITierlist[]}) {
+    if (props.tierlists.length == 0) return <Typography>0 tierlists</Typography>;
     return (<>
         {
             props.tierlists.map((tierlist)=>(
